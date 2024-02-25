adc687f429910a0d5f073edcc7026c14996bceea
Shreya POTHKANOORI <shreya.pothkanoori@hexagon.com>
2024-02-26 00:06:33 +0530

    Added Map markers with gallery carousel and time slider

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,13 +1,26 @@
 import styles from './Map.module.css';
 import { MapContainer,TileLayer,Marker,Popup } from 'react-leaflet';
+import DateSlider from './DateSlider';
+import { useData } from '../Contexts/DataContext';
+import Carousel from 'react-gallery-carousel';
+import 'react-gallery-carousel/dist/index.css';
+
 
 function Map(){
+
+  const {data} = useData();
+
+  const images = ["amaravathi","amaravathi1"].map((number) => ({
+    src: `/${number}.jpg`
+  }));
+
     return(
         <div className={styles.mapContainer}>
+          <DateSlider/>
 
 <MapContainer 
-center={[20.932142, 77.746864]} 
-zoom={13} 
+center={[ 16.513494544336695, 80.51625036169007]} 
+zoom={11} 
 scrollWheelZoom={false}
 className={styles.map}
 >
@@ -15,11 +28,15 @@ className={styles.map}
     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
   />
-  <Marker position={[51.505, -0.09]}>
+  {data.map((place)=>
+  <Marker position={[place.position.lat, place.position.lng]}>
     <Popup>
-      A pretty CSS3 popup. <br /> Easily customizable.
+      
+    {place.city}
+      <Carousel images={images} style={{ height: 250, width: 250 }} />
+      
     </Popup>
-  </Marker>
+  </Marker>)}
 </MapContainer>
         </div>
     )
